dd6319fee7e1e5736b6c1979728b692111191ac1
Alva Widerberg Palmfeldt <alvawiderberg.palmfeldt@se.experis.com>
2023-08-29 16:13:55 +0200

    Updated website 08-29

---
+++ js/loadElements.js
@@ -0,0 +1,18 @@
+
+
+
+// footer content
+let footerHtml = `
+    <p>E-Mailaddress: <a href="mailto:alva@palmfeldt.se">alva@palmfeldt.se</a></p>
+    <div id="linkedIn">
+      <p>My LinkedIn
+        <a href="https://www.linkedin.com/in/alva-widerberg-palmfeldt-393a41261/">
+          <img class="socialMediaIcon" src="/img/linked.png" alt="linkedin-logo">
+        </a>
+      </p>
+    </div>
+    <p>Latest website update: 2023-08</p>
+    <p class="tiny">I'll probably forget to change this date, hehe</p>
+`;
+
+document.querySelectorAll('footer')[0].innerHTML = footerHtml;
