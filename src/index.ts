07f7adcf2d49547e1a84a11c20f148036824365d
Alva Widerberg Palmfeldt <alva.palmfeldt@aveki.se>
2025-03-13 18:32:26 +0100

    Feat: now dynamically renders the md file

--- src/index.ts
+++ src/index.ts
@@ -15,16 +15,40 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("frontend"));
+
+app.use(express.static(path.join(__dirname, "frontend", "static")));
+
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("frontend/index.html");
+  const indexPath = path.join(__dirname, "frontend", "index.html");
+  const directoryPath = path.join(__dirname, "MD-files");
+
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error("Could not read md files");
+    }
+
+    const markdownFiles = files.filter(file => path.extname(file.toLowerCase()) === ".md");
+    let htmlContent = "";
+
+    markdownFiles.forEach((file, index) => {
+      const filePath = path.join(directoryPath, file);
+      const markdownData = fs.readFileSync(filePath, "utf8");
+      htmlContent += "<article>" + marked(markdownData) + "</article>";
+
+    });
+    fs.readFile(indexPath, "utf8", (err, data) => {
+
+      const modifiedHtml = data.replace('<div class="replacer"></div>', htmlContent);
+      res.send(modifiedHtml);
+    });
+  });
 });
 
 // Route to render Markdown files as HTML
 app.get("/markdown/:filename", (req: Request, res: Response) => {
   const filename = req.params.filename;
-  const filePath = path.join( "MD-files", `${filename}.md`);
+  const filePath = path.join("MD-files", `${filename}.md`);
 
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
