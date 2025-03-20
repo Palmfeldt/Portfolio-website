ff68f373e36ccdae68cb0e32535edd008afdce1a
Alva Widerberg Palmfeldt <alva.palmfeldt@aveki.se>
2025-03-20 08:48:14 +0100

    Bug: fixed invalid path for 404

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from "express";
-import path from "path";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const handleNotFound = (req: Request, res: Response, next: NextFunction) => {
-  res.status(404).sendFile(path.join(__dirname, "../../frontend/static", "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "../public/static", "404.html"));
 };
